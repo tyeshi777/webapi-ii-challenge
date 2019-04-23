67b0659dbae4bd1a5d34ce0b1f652b3326e3f034
Yeshi <tyeshi777@yahoo.com>
2019-04-23 15:39:44 -0700

    all routes working

--- data/db-router.js
+++ data/db-router.js
@@ -18,7 +18,11 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const postBody = req.body;
-
+  if (!req.body.title && req.body.contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post" });
+  }
   Db.insert(postBody)
     .then(post => {
       res.status(201).json(post);
@@ -30,6 +34,29 @@ router.post("/", (req, res) => {
     });
 });
 
+router.delete("/:id", (req, res) => {
+  const postId = req.params.id;
+  Db.remove(postId)
+    .then(deleted => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      res.status(500).json({ err: "The post cannot be removed" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const postId = req.params.id;
+  const postBody = req.body;
+
+  Db.update(postId, postBody)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "The post information cannot be modified" });
+    });
+});
 router.get("/:id", (req, res) => {
   const postId = req.params.id;
   Db.findById(postId)
